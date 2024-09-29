bc0110fd8eeb6b5798abb7a78da8d0b816f84131
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-09-29 19:50:13 -0300

    feat: error log

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -2,6 +2,10 @@ import { BaseException } from '../exceptions/base-exception';
 
 export const errorHandler = (error: Error, req: any, res: any, next: any) => {
 	if (error instanceof BaseException) {
+		console.error(
+			`[${req.method}] - ${req.url} :>> ${error.errorCode} - ${error.stack}`,
+		);
+
 		return res.status(error.errorCode).json({
 			code: error.errorCode,
 			message: error.message,
